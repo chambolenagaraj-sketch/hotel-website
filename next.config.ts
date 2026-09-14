4b91d63020a362262dfe64cd2a6dc9b263373d55
chambolenagaraj-sketch <chambolenagaraj@gmail.com>
2026-09-14 10:47:24 +0530

    Remove output configuration from next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: 'export',
   images: { unoptimized: true },
   trailingSlash: true,
   // basePath: '/your-repo-name', // uncomment if not using a custom domain or user page
